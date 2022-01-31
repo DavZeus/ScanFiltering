5f44b50766b333562ef7f828a68f5f1dac46d254
DavZeus <dew.zhibin@yandex.ru>
2022-01-31 18:56:16 +0300

    feat: add more filters

--- ScanFiltering/main.cpp
+++ ScanFiltering/main.cpp
@@ -3,124 +3,65 @@
 #include <fmt/core.h>
 #include <array>
 
+using namespace cv;
+
 const std::string_view window_name = "1";
 
-void show_image(cv::Mat img) {
-	cv::imshow(window_name.data(), img);
-	cv::waitKey(0);
+void show_image(Mat img) {
+	imshow(window_name.data(), img);
+	waitKey(0);
 }
 
 template<class F, class ...Args>
-cv::Mat apply_filter(cv::Mat img, bool show, F func, Args... args) {
-	cv::Mat filtered_img;
+Mat apply_filter(Mat img, bool show, F func, Args... args) {
+	Mat filtered_img;
 	func(img, filtered_img, std::forward<Args>(args)...);
 	if (show) show_image(filtered_img);
 	return filtered_img;
 }
 
-
-//cv::Mat apply_blur(cv::Mat img, bool show = true) {
-//	cv::Mat filtered_img;
-//	cv::blur(img, filtered_img, );
-//	if (show) show_image(filtered_img);
-//	return filtered_img;
-//}
-//
-//cv::Mat apply_box_filter(cv::Mat img, bool show = true) {
-//	cv::Mat filtered_img;
-//	cv::boxFilter(img, filtered_img, );
-//	if (show) show_image(blur_img);
-//	show_image(filtered_img);
-//	return filtered_img;
-//}
-//
-//cv::Mat apply_median_blur(cv::Mat img, bool show = true) {
-//	cv::Mat filtered_img;
-//	cv::medianBlur(img, filtered_img, );
-//	if (show) show_image(blur_img);
-//	show_image(filtered_img);
-//	return filtered_img;
-//}
-//
-//cv::Mat apply_gaussian_blur(cv::Mat img, bool show = true) {
-//	cv::Mat filtered_img;
-//	const int kernel = 9;
-//	cv::GaussianBlur(img, filtered_img, { kernel, kernel }, 0);
-//	if (show) show_image(blur_img);
-//	show_image(filtered_img);
-//	return filtered_img;
-//}
-//
-//cv::Mat apply_bilateral_blur(cv::Mat img, bool show = true) {
-//	cv::Mat bilateral_img;
-//	int d = 9;
-//	double sigma = 50;
-//	cv::bilateralFilter(img, bilateral_img, d, sigma, sigma);
-//	if (show) show_image(blur_img);
-//	show_image(bilateral_img);
-//	return bilateral_img;
-//}
-//
-//cv::Mat apply_erode(cv::Mat img, bool show = true) {
-//	cv::Mat erode_img;
-//	cv::erode(img, erode_img, );
-//	if (show) show_image(blur_img);
-//	show_image(erode_img);
-//	return erode_img;
-//}
-//
-//cv::Mat apply_dilate(cv::Mat img, bool show = true) {
-//	cv::Mat dilate_img;
-//	cv::dilate(img, dilate_img, );
-//	if (show) show_image(blur_img);
-//	return dilate_img;
-//}
-//
-cv::Mat detect_edges(cv::Mat img, bool show = true) {
-	cv::Mat edges;
+Mat detect_edges(Mat img, bool show = true) {
+	Mat edges;
 	const double low_threshold = 240;
 	const double high_threshhold = 250;
-	cv::Canny(img, edges, low_threshold, high_threshhold, 3);
+	Canny(img, edges, low_threshold, high_threshhold, 3);
 	if (show) show_image(edges);
 	return edges;
 }
 
-cv::Mat cvt_non_white_to_black(cv::Mat img, bool show = true) {
-	cv::Mat bw_img = img.clone();
+Mat cvt_non_white_to_black(Mat img, bool show = true) {
+	Mat bw_img = img.clone();
 	bw_img.forEach<uint8_t>([](uint8_t& p, const int* pos) {
 		if (p != 255) p = 0;
-		});
+	});
 	if (show) show_image(bw_img);
 	return bw_img;
 }
 
 int main() {
-	cv::namedWindow(window_name.data(), cv::WindowFlags::WINDOW_KEEPRATIO);
+	using namespace cv;
+	namedWindow(window_name.data(), WindowFlags::WINDOW_KEEPRATIO);
 
-	cv::Mat img = cv::imread("20k_edge.bmp");
+	Mat img = imread("20k_edge.bmp");
 	show_image(img);
 
-	cv::Mat gray_img;
-	cv::cvtColor(img, gray_img, cv::COLOR_RGB2GRAY);
-
-	std::array filtered_images = {
-		apply_filter(img, true, &cv::blur, cv::Size(3, 3), cv::Point(-1, -1), cv::BORDER_DEFAULT),
-		apply_filter(img, true, &cv::bilateralFilter, 9, 50, 50, cv::BORDER_DEFAULT),
-	};
-
+	Mat gray_img;
+	cvtColor(img, gray_img, COLOR_RGB2GRAY);
 	detect_edges(gray_img);
 
-	/*const double rho = 1;
-	constexpr double theta = std::numbers::pi / 180;
-	const int threshold = 15;
-	const double min_line_length = 5;
-	const double max_line_gap = std::min(img.size().height, img.size().width) / 100.;
-	std::vector<cv::Vec4i> lines;
-	cv::HoughLinesP(edges, lines, rho, theta, threshold, min_line_length, max_line_gap);
-	for (const auto& line : lines) {
-		const auto [x0, y0, x1, y1] = line.val;
-		cv::line(img, { x0, y0 }, { x1, y1 }, { 255, 0, 0 }, 1);
+	std::array filtered_images = {
+		//apply_filter(gray_img, true, &filter2D, gray_img.type(), Mat::ones(3, 3, CV_32F) / static_cast<float>(9), Point(-1, -1), 0, BORDER_DEFAULT),
+		//apply_filter(gray_img, true, &blur, Size(3, 3), Point(-1, -1), BORDER_DEFAULT),
+		//apply_filter(gray_img, true, &boxFilter, gray_img.type(), Size(3, 3), Point(-1, -1), true, BORDER_DEFAULT),
+		//apply_filter(gray_img, true, &bilateralFilter, 9, 50, 50, BORDER_DEFAULT),
+		//apply_filter(gray_img, true, &GaussianBlur, cv::Size(3, 3), 0, 0, BORDER_DEFAULT),
+		//apply_filter(gray_img, true, &medianBlur, 3),
+		//apply_filter(gray_img, true, &dilate, )
+	};
+
+	for (const auto img : filtered_images) {
+		detect_edges(img);
+		const auto bw_img = cvt_non_white_to_black(img);
+		detect_edges(bw_img);
 	}
-	cv::imshow(window_name.data(), img);
-	cv::waitKey(0);*/
 }
