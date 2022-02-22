b40036f67ae6fa7d120a18fcb73454b42be91700
DavZeus <dew.zhibin@gmail.com>
2022-02-22 13:38:50 +0300

    feat: save data as csv file

--- ScanFiltering/main.cpp
+++ ScanFiltering/main.cpp
@@ -1,14 +1,20 @@
 #include <filesystem>
 #include <fmt/core.h>
+#include <fstream>
 #include <functional>
+#include <map>
 #include <numeric>
-#include <opencv2/opencv.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <vector>
 
+#define _STR(x) "_" #x
+#define STR(x) #x
 using namespace cv;
 
-#define PRINT_TYPE(x) fmt::print("Img type: {}", x);
-
 const std::string_view window_name = "1";
+const std::string_view data_filename = "big_data.csv";
 
 void show_image(Mat img) {
   imshow(window_name.data(), img);
@@ -50,8 +56,7 @@ Mat apply_closer(Mat img, bool show = true) {
 }
 
 Mat apply_opening(Mat img, bool show = true) {
-  // Mat kernel = Mat::ones(3, 3, img.type());
-  Mat kernel = (Mat_(3, 3, CV_8SC1) << 1, 1, 1, 1, -8, 1, 1, 1, 1);
+  Mat kernel = Mat::ones(3, 3, img.type());
   Point anchor(-1, -1);
   int iteration_number = 3;
   return apply_filter(img, show, &morphologyEx, MORPH_OPEN, kernel, anchor,
@@ -132,7 +137,7 @@ std::string make_save_folder(std::string path = "") {
   return path;
 }
 
-void save_image(Mat img, std::string path, std::string filename) {
+void save_image(Mat img, const std::string &path, const std::string &filename) {
   std::filesystem::path fullname{path};
   fullname /= filename + ".png";
   imwrite(fullname.string(), img);
@@ -148,7 +153,7 @@ Mat detect_edges(Mat img, bool show = true) {
   return edges;
 }
 
-Mat cvt_non_white_to_black(Mat img, bool show = true) {
+Mat cvt_to_bw(Mat img, bool show = true) {
   Mat bw_img = img.clone();
   bw_img.forEach<uint8_t>([](uint8_t &p, const int *pos) {
     if (p != 255)
@@ -159,45 +164,117 @@ Mat cvt_non_white_to_black(Mat img, bool show = true) {
   return bw_img;
 }
 
-void avg(Mat img, std::string folder, std::string save_name) {
-  std::vector<int> points(img.rows);
-  for (int i = 0; i < img.rows; ++i) {
-    Mat_<uchar> row = img.row(i);
-    auto first = std::find(row.begin(), row.end(), 255);
-    auto last = std::find(row.rbegin(), row.rend(), 255);
-    if (first != row.end() && last != row.rend()) {
-      points.emplace_back(
-          (first.pos() + (last.base().pos() - first.pos()) / 2).x);
+std::vector<Point> find_line_points(Mat img) {
+  std::vector<Point> line_points;
+  line_points.reserve(img.cols);
+  for (int i = 0; i < img.cols; ++i) {
+    Mat_<uchar> col = img.col(i);
+    auto first = std::find(col.begin(), col.end(), 255);
+    auto last = std::find(col.rbegin(), col.rend(), 255);
+    if (first != col.end() && last != col.rend()) {
+      line_points.emplace_back(i, ((last.base().pos() + first.pos()) / 2).y);
     }
   }
-  if (!points.empty()) {
-    int average =
-        std::accumulate(points.begin(), points.end(), 0) / points.size();
-    std::vector<int> deviation(points.size());
-    for (auto p : points) {
-      deviation.emplace_back(std::abs(p - average));
-    }
-    std::max
+  line_points.shrink_to_fit();
+  return line_points;
+}
+
+void draw_line(Mat img, const std::string &folder, const std::string &save_name,
+               std::vector<Point> points) {
+  if (points.empty()) {
+    return;
   }
+  Mat img_with_line;
+  cvtColor(img, img_with_line, COLOR_GRAY2BGR);
+  for (auto p1 = points.begin(), p2 = points.begin() + 1; p2 != points.end();
+       ++p1, ++p2) {
+    line(img_with_line, *p1, *p2, {0, 0, 255});
+  }
+  // To draw only points
+  // for (const auto &p : points) {
+  //  line(img_with_line, p, p, {0, 0, 255});
+  // }
+  save_image(img_with_line, folder, save_name + "_line");
+}
+
+auto form_data(const std::vector<Point> &line_points) {
+  const int average_point =
+      std::reduce(line_points.begin(), line_points.end()).y /
+      static_cast<int>(line_points.size());
+  std::vector<int> deviation;
+  deviation.reserve(line_points.size());
+  for (auto p : line_points) {
+    deviation.emplace_back(std::abs(p.y - average_point));
+  }
+  const int max_deviation =
+      *std::max_element(deviation.begin(), deviation.end());
+  const int average_deviation =
+      std::reduce(deviation.begin(), deviation.end()) /
+      static_cast<int>(deviation.size());
+  std::map<std::string, int> data;
+  data.emplace(STR(average_point), average_point);
+  data.emplace(STR(max_deviation), max_deviation);
+  data.emplace(STR(average_deviation), average_deviation);
+  return data;
+}
+
+void write_data(const std::string &folder, std::map<std::string, int> data) {
+  std::filesystem::path full_path(folder);
+  full_path /= data_filename;
+  std::ofstream f(full_path, std::ios::ate);
+  if (!f) {
+    fmt::print("Can not open data file\n");
+    return;
+  }
+  f.imbue(std::locale("ru_RU.UTF-8"));
+  for (const auto &[key, value] : data) {
+    f << key << ": " << value << ';';
+  }
+  f << '\n';
+}
+
+// TODO: Rewrite to use map instead of vector
+void make_data(Mat img, const std::string &folder,
+               const std::string &save_name) {
+  const std::vector<Point> line_points = find_line_points(img);
+  if (line_points.empty()) {
+    fmt::print("No points found in {}\n", save_name);
+    return;
+  }
+  write_data(folder, save_name, form_data(line_points));
+  draw_line(img, folder, save_name, line_points);
 }
 
 template <class F>
 void filter_img(Mat img, std::string folder, std::string save_name, F func,
                 bool show = true) {
+  Mat bw_img = cvt_to_bw(img, show);
   Mat f_img = func(img, show);
   Mat e_f_img = detect_edges(f_img, show);
-  Mat bw_f_img = cvt_non_white_to_black(f_img, show);
+  Mat bw_f_img = cvt_to_bw(f_img, show);
   Mat e_bw_f_img = detect_edges(bw_f_img, show);
-  Mat otsu_f_img;
-  threshold(f_img, otsu_f_img, 0, 255, THRESH_BINARY | THRESH_OTSU);
-  Mat e_otsu_f_img = detect_edges(bw_f_img, show);
+  // Mat otsu_f_img;
+  // threshold(f_img, otsu_f_img, 0, 255, THRESH_BINARY | THRESH_OTSU);
+  // Mat e_otsu_f_img = detect_edges(bw_f_img, show);
 
-  save_image(f_img, folder, save_name);
-  save_image(e_f_img, folder, save_name + "_edges");
-  save_image(bw_f_img, folder, save_name + "_bw");
-  save_image(e_bw_f_img, folder, save_name + "_bw_edges");
-  save_image(otsu_f_img, folder, save_name + "_otsu");
-  save_image(e_otsu_f_img, folder, save_name + "_otsu_edges");
+  save_image(bw_img, folder, save_name + _STR(bw_img));
+  save_image(f_img, folder, save_name + _STR(f_img));
+  save_image(e_f_img, folder, save_name + _STR(e_f_img));
+  save_image(bw_f_img, folder, save_name + _STR(bw_f_img));
+  save_image(e_bw_f_img, folder, save_name + _STR(e_bw_f_img));
+  // save_image(otsu_f_img, folder, save_name + _STR(otsu_f_img));
+  // save_image(e_otsu_f_img, folder, save_name + _STR(e_otsu_f_img));
+
+  make_data(bw_img, folder, save_name + _STR(bw_img));
+  make_data(bw_f_img, folder, save_name + _STR(bw_f_img));
+  // avg(otsu_f_img, folder, save_name + _STR(otsu_f_img));
+}
+
+void make_csv(const std::string &folder) {
+  std::filesystem::path fullpath(folder);
+  fullpath /= data_filename.data();
+  std::ofstream f(fullpath);
+  f << ";Average point;Max deviation;Average deviation\n";
 }
 
 int main(int argc, char *argv[]) {
@@ -217,6 +294,7 @@ int main(int argc, char *argv[]) {
   cvtColor(imread(argv[1]), img, COLOR_RGB2GRAY);
 
   std::string folder = make_save_folder();
+  make_csv(folder);
   Mat thr;
   threshold(img, thr, 0, 255, THRESH_BINARY | THRESH_OTSU);
   save_image(thr, folder, "otsu");
