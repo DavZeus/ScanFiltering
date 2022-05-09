f7b7abe8e16b7cb16fcef25c5ba09ca38477c1df
Дэйвидас Жибинскас <dew.zhibin@gmail.com>
2022-05-09 21:16:27 +0300

    fix: process not found case

--- ScanFiltering/custom_utility.cpp
+++ ScanFiltering/custom_utility.cpp
@@ -4,7 +4,8 @@ namespace cu {
 void remove_wrong_ru_separator(std::string &str) {
   size_t pos = 0;
   char wrong_separator[] = "Â";
-  while (auto found_pos = str.find(wrong_separator, pos)) {
+  size_t found_pos{};
+  while (found_pos = str.find(wrong_separator, pos) != std::string::npos) {
     str.erase(found_pos, sizeof wrong_separator);
     pos = found_pos;
   }
